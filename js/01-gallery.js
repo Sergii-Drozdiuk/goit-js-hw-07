bee348b5c8afb93febb6300a912b71b28a5f34de
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-08-03 02:32:02 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,37 +3,49 @@ import { galleryItems } from './gallery-items.js';
 
 const container = document.querySelector('.gallery');
 container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
-container.addEventListener('click', handlerClick)
+container.addEventListener('click', handlerClick);
 
 function createMarkup(arr) {
-   return arr.map(({ preview, original, description }) => `
-   <li class="gallery__item">
-      <a class="gallery__link" href="${original}">
-         <img
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
             class="gallery__image"
             src="${preview}"
             data-source="${original}"
             alt="${description}"
-         />
-      </a>
-   </li>`).join('');
+          />
+        </a>
+      </li>
+    `
+    )
+    .join('');
 }
 
-let instance = null
+let instance = null;
 
 function handlerClick(evt) {
-   evt.preventDefault();
-   if (evt.target.nodeName !== 'IMG') {
-        return;
-   }
-   instance = basicLightbox.create(`<img src="${evt.target.dataset.source}">`);
-   instance.show();
-   document.addEventListener('keydown', closeOnEsc);
+  evt.preventDefault();
+  const targetImg = evt.target.closest('img');
+  if (!targetImg) {
+    return;
+  }
+  
+  instance = basicLightbox.create(`<img src="${targetImg.dataset.source}">`, {
+    onShow: () => {
+      document.addEventListener('keydown', closeOnEsc);
+     },
+     onClose: () => {
+      document.removeEventListener('keydown', closeOnEsc);
+    },
+  });
+  instance.show();
 }
 
 function closeOnEsc(evt) {
   if (evt.key === 'Escape') {
     instance.close();
-    document.removeEventListener('keydown', closeOnEsc);
   }
 }
